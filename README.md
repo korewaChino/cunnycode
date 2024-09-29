8683a7978d72110d18b7675bd2052d7cb20dfff5
Cappy Ishihara <cappy@cappuchino.xyz>
2024-09-30 02:14:50 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# cunnycode
+
+Cunnycode is a simple Rust program that converts a string into cunnycode (Morse w/ Emojis).
+
+## Usage
+
+```sh
+cunnycode -m to "Hello, World!"
+
+```
+
+Inspired by [this tweet](https://x.com/SethC1995/status/1839472034721456176)
+
+## License
+
---
+++ src/main.rs
@@ -0,0 +1,70 @@
+use clap::{Parser, ValueEnum};
+use clap_stdin::MaybeStdin;
+
+const DOT: char = '😭';
+const DASH: char = '💢';
+
+fn into_ccode(input: &str) -> String {
+    format!(
+        "^{}",
+        crypto_morse::encode(input)
+            .chars()
+            .map(|c| match c {
+                '.' => DOT,
+                '_' => DASH,
+                _ => c,
+            })
+            .collect::<String>()
+            .replace("/ ", "^")
+    )
+}
+
+fn from_ccode(input: &str) -> String {
+    let i = input
+        .trim_start_matches("^")
+        .replace(DOT, ".")
+        .replace(DASH, "_")
+        .replace("^", "/ ");
+        // remove invalid characters that aren't utf8
+
+    crypto_morse::decode(&i)
+}
+
+#[derive(ValueEnum, Debug, Clone)]
+enum Mode {
+    To,
+    From,
+}
+
+#[derive(Parser, Debug)]
+struct Cli {
+    #[clap(value_enum, short, long)]
+    mode: Mode,
+    input: MaybeStdin<String>,
+}
+fn main() {
+    let args = Cli::parse();
+
+    match args.mode {
+        Mode::To => {
+            println!("{}", into_ccode(&args.input));
+        }
+        Mode::From => {
+            println!("{}", from_ccode(&args.input));
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_proper_conversion() {
+        let input = "hello world!!!";
+        let ccode = into_ccode(input);
+        let output = from_ccode(&ccode);
+        assert_eq!(input, output);
+    }
+}
